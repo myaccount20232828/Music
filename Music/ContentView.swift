54e19be8e9c2b16c62a190fb1bb7209a31a02d65
myaccount20232828 <146480147+myaccount20232828@users.noreply.github.com>
2024-05-28 22:06:54 -0400

    Update ContentView.swift

--- Music/ContentView.swift
+++ Music/ContentView.swift
@@ -12,19 +12,19 @@ struct ContentView: View {
                     Spacer()
                     ForEach(Songs.prefix(20)) { Song in
                         Button {
-                            let alert = UIAlertController(title: "Video ID", message: "Put in the YouTube video ID here.", preferredStyle: .alert)
-                            alert.addTextField(configurationHandler: { (textField) -> Void in
-                                textField.text = UIPasteboard.general.string ?? ""
+                            let Alert = UIAlertController(title: "Video ID", message: "Put in the YouTube video ID here.", preferredStyle: .alert)
+                            Alert.addTextField(configurationHandler: { (textField) -> Void in
+                                textField.text = GetVideoID(UIPasteboard.general.string ?? "")
                                 textField.placeholder = "Video ID"
                             })
-                            alert.addTextField(configurationHandler: { (textField) -> Void in
+                            Alert.addTextField(configurationHandler: { (textField) -> Void in
                                 textField.text = "0"
                                 textField.placeholder = "Start Time"
                             })
-                            alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
-                            alert.addAction(UIAlertAction(title: "Add", style: .default, handler: { [weak alert] (action) -> Void in
-                                let Video = ((alert?.textFields![0])! as UITextField).text ?? ""
-                                let StartTime = Double(((alert?.textFields![1])! as UITextField).text ?? "0") ?? 0
+                            Alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+                            Alert.addAction(UIAlertAction(title: "Add", style: .default, handler: { [weak Alert] (action) -> Void in
+                                let Video = GetVideoID((Alert?.textFields![0])! as UITextField).text ?? "")
+                                let StartTime = Double(((Alert?.textFields![1])! as UITextField).text ?? "0") ?? 0
                                 DispatchQueue.global(qos: .utility).async {
                                     ShowAlert(UIAlertController(title: "Creating \(Song.trackName)", message: "", preferredStyle: .alert))
                                     let Result = MakeSong(Song, Video, StartTime)
@@ -35,7 +35,7 @@ struct ContentView: View {
                                     }
                                 }
                             }))
-                            ShowAlert(alert)
+                            ShowAlert(Alert)
                         } label: {
                             ZStack {
                                 Gray
@@ -109,6 +109,12 @@ struct ContentView: View {
     }
 }
 
+let DarkGray = Color(Hex: "0D1117")
+let Gray = Color(Hex: "161B22")
+let Gray2 = Color(Hex: "89929B")
+let LightGray = Color(Hex: "21262D")
+let AppColor = Color(Hex: "7950F2")
+
 func ShowAlert(_ Alert: UIAlertController) {
     DispatchQueue.main.async {
         UIApplication.shared.windows.first { $0.isKeyWindow }?.rootViewController?.present(Alert, animated: true)
@@ -121,11 +127,13 @@ func Dismiss() {
     }
 }
 
-let DarkGray = Color(Hex: "0D1117")
-let Gray = Color(Hex: "161B22")
-let Gray2 = Color(Hex: "89929B")
-let LightGray = Color(Hex: "21262D")
-let AppColor = Color(Hex: "7950F2")
+func GetVideoID(_ Video: String) -> String {
+    if Video.contains("youtu.be") {
+        return Video.components(separatedBy: "youtu.be/")[1].components(separatedBy: "?")[0]
+    } else {
+        return Video
+    }
+}
 
 extension Color {
     init(Hex: String, Opacity: CGFloat = 1.0) {

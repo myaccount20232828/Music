4f893851816c2b3f26b876de1f273bc88f723e55
myaccount20232828 <146480147+myaccount20232828@users.noreply.github.com>
2024-05-28 20:58:23 -0400

    Update ContentView.swift

--- Music/ContentView.swift
+++ Music/ContentView.swift
@@ -4,15 +4,12 @@ struct ContentView: View {
     @State var Songs: [MusicItem] = []
     @State var Search = ""
     var body: some View {
-        NavigationView {
-            Form {
-                ForEach(Songs) { Song in
-                    Text(Song.trackName)
-                }
+        Form {
+            ForEach(Songs) { Song in
+                Text(Song.trackName)
             }
-            .searchable(text: $Search)
         }
-        .navigationViewStyle(.stack)
+        .searchable(text: $Search)
         .onChange(of: Search) { _ in
             DispatchQueue.global(qos: .utility).async {
                 Songs = SearchSongs(Search)

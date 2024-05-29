a5aa41620daf19308aebc9a350a46a6a205a1b0b
myaccount20232828 <146480147+myaccount20232828@users.noreply.github.com>
2024-05-28 20:49:53 -0400

    Update ContentView.swift

--- Music/ContentView.swift
+++ Music/ContentView.swift
@@ -1,10 +1,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var Songs: [MusicItem] = []
+    @State var Search = ""
     var body: some View {
         NavigationView {
             Form {
-                Text("Music")
+                ForEach(Songs) { Song in
+                    Text(Song.trackName)
+                }
+            }
+        }
+        .onChange(of: Search) { _ in
+            DispatchQueue.global(qos: .utility).async {
+                Songs = SearchSongs(Search)
             }
         }
     }

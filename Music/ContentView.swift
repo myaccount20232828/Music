42f56b87bb33224dfc7dd00b4d9dcbb8550999a6
myaccount20232828 <146480147+myaccount20232828@users.noreply.github.com>
2024-05-30 01:33:50 -0400

    Update ContentView.swift

--- Music/ContentView.swift
+++ Music/ContentView.swift
@@ -101,7 +101,7 @@ struct ContentView: View {
             }
             .toolbar {
                 ToolbarItem(placement: .principal) {
-                    Text("Music 17")
+                    Text("Music 18")
                         .font(.system(size: 18, weight: .semibold, design: .rounded))
                         .foregroundColor(Color.white)
                 }
@@ -205,7 +205,7 @@ struct PlayerView: View {
                     }
                 }
                 .padding(30)
-                HStack(spacing: 25) {
+                HStack(spacing: 35) {
                     Button {
                         withAnimation {
                             MP.PlayPreviousSong()
